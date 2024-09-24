cb62578345003a86e1e0589535d5ba27d8799a6b
JulienMagnier <julien.magnier@std.heh.be>
2024-09-24 10:01:03 +0200

    Ajout du main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#from sommeFunc import calculer_somme
+#from moyenneFunc import calcul_moyenne
+
+nombres = [1.0, 2.0, 3.0]
+print("Data list :")
+print(nombres)
+
+print("---------------")
+#somme, nombre_elements = calculer_somme(nombres)
+#print("Somme :", somme)
+#print("Nombre d'éléments :", nombre_elements)
+
+print("---------------")
+#moyenne, min_val, max_val = calcul_moyenne(nombres)
+#print("Moyenne :", moyenne)
+#print("Min :", min_val)
+#print("Max :", max_val)
